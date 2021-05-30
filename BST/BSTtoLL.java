c37c4f62d9008a0ef083bb88957cb57591ed541b
newatcoding <thakurlakshay2@gmail.com>
2021-05-30 18:48:31 +0530

    BST to LL

--- BST/BSTtoLL.java
+++ BST/BSTtoLL.java
@@ -1,11 +1,13 @@
 public class BSTtoLL {
-    public static void BSTtoDLL(Node root ){
+    static node head;
+    static node tail;
+    public static void BSTtoDLL(node root ){
         if (root == null)
-      return;
+            return;
     
         // First convert the left subtree
         if (root.left != null)
-        convertBSTtoDLL(root.left);
+            convertBSTtoDLL(root.left);
     
         // Then change left of current root
         // as last node of left subtree
@@ -15,17 +17,44 @@ public class BSTtoLL {
         // of tail as root, else current
         // node is head
         if (tail != null)
-        (tail).right = root;
+            (tail).right = root;
         else
-        head = root;
+            head = root;
     
         // Update tail
         tail = root;
     
         // Finally, convert right subtree
         if (root.right != null)
-        convertBSTtoDLL(root.right);
+            convertBSTtoDLL(root.right);
     }
+   
+
+    static Node BSTToSortedLL(Node root, Node head_ref)
+    {
+        // Base cases
+        if(root == null)
+            return head_ref;
+    
+        // Recursively convert right subtree
+        head_ref = BSTToSortedLL(root.right, head_ref);
+    
+        // insert root into linked list
+        root.right = head_ref;
+    
+        // Change left pointer of previous head
+        // to point to null
+        if (head_ref != null)
+            (head_ref).left = null;
+    
+        // Change head of linked list
+        head_ref = root;
+    
+        // Recursively convert left subtree
+        head_ref = BSTToSortedLL(root.left, head_ref);
+        return head_ref;
+    }
+
     public static void main(String[] args) {
         
     }
