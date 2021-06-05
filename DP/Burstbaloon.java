980b4cd40ddd5774f669ce4e72242a7f9ebe2ae5
newatcoding <thakurlakshay2@gmail.com>
2021-06-05 16:43:42 +0530

    Buy ans sell all posiblity

--- DP/Burstbaloon.java
+++ DP/Burstbaloon.java
@@ -22,23 +22,29 @@ public class Burstbaloon {
         int[][] dp=new int[arr.length][arr.length];
         for (int[] row : dp) 
             Arrays.fill(row, -1); 
-        return burst(arr,dp,0,arr.length-1);
+        return burst(arr,dp,1,arr.length-2);
     }
     
     public int burst(int[] arr,int[][] dp,int left,int right){
-        if(left+1==right){
-            return 0;
-        }
+        // if(left+1==right){
+        //     return 0;
+        // }
         if(dp[left][right]!=-1){
             return dp[left][right];
         }
         int ans=0;
-        for(int i=left+1;i<right;i++){
-            int temp1=burst(arr,dp,left,i);
-            int temp2=burst(arr,dp,i,right);
-            int tempans=arr[left]*arr[right]*arr[i];
-            ans=Math.max(ans,temp1+temp2+tempans);
+        for(int i=left;i<=right;i++){
+            // int temp1=burst(arr,dp,left,i);
+            // int temp2=burst(arr,dp,i,right);
+            int tempans=arr[i-1]*arr[i+1]*arr[i];
+            int val=arr[i];
+            arr[i]=1;
+            int temp1=burst(arr,dp,left+1,right);
+            arr[i]=val;
+            int temp2=burst(arr,dp,left+1,right);
+            ans=Math.max(ans,Math.max(temp1+tempans,temp2+arr[i]));
         }
+        System.out.println(ans+"  "+left+"  "+right);
         dp[left][right]=ans;
         return ans;
     }
