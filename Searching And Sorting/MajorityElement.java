70cab977b7c808ff4cddf9d87f8fab69f521aa5f
newatcoding <thakurlakshay2@gmail.com>
2021-06-16 14:17:54 +0530

    Moore's Voting

---
+++ Searching And Sorting/MajorityElement.java
@@ -0,0 +1,26 @@
+public class MajorityElement {
+    // Moor's Voting Algorithm
+    public int findMajor(int[] arr){
+        int n=arr.length;
+        int freq=1;
+        int val=arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]==val){
+                freq++;
+            }else{
+                freq--;
+                if(freq==0){
+                    val=arr[i];
+                    freq++;
+                }
+            }
+        }
+        return val;
+    }
+    public static void main(String[] args) {
+        int[] arr=new int[]{1,1,2,3,4,1,1,4,1,5,1};
+        MajorityElement b=new MajorityElement();
+        
+        System.out.printlb(b.findMajor(arr));
+    }
+}
